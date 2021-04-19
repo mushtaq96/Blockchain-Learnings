a4a30a034867b9920ef236fdffd1e9b73d0fe3df
mushtaq96 <mushtaq96smb@gmail.com>
2021-04-20 00:40:29 +0900

    basic working blockchain with Pow - done

--- java_blockchain/noobchain/src/Block.java
+++ java_blockchain/noobchain/src/Block.java
@@ -5,6 +5,7 @@ public class Block {
     public String previousHash;
     private String data;
     private long timestamp;
+    private int nonce;
 
     public Block(String data, String previousHash) {
 
@@ -15,7 +16,17 @@ public class Block {
     }
 
     public String calculateHash() {
-        String calculatedHash = StringUtil.applySHA256(previousHash + Long.toString(timestamp) + data);
+        String calculatedHash = StringUtil
+                .applySHA256(previousHash + Long.toString(timestamp) + Integer.toString(nonce) + data);
         return calculatedHash;
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("block mined!!" + hash);
     }
 }
--- java_blockchain/noobchain/src/NoobChain.java
+++ java_blockchain/noobchain/src/NoobChain.java
@@ -1,12 +1,47 @@
+import java.util.ArrayList;
+
+import com.google.gson.GsonBuilder;
+
 public class NoobChain {
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static int difficulty = 3;
+
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("current hashes are not equal");
+                return false;
+            }
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("previous hashes are not equal");
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        Block genesisBlock = new Block("hi i am the first block", "0");
-        System.out.println("Hash for block 1 : " + genesisBlock.hash);
+        blockchain.add(new Block("hi i am the first block", "0"));
+        System.out.println("trying to mine block 1...");
+        blockchain.get(0).mineBlock(difficulty);
 
-        Block secondBlock = new Block("hi i am the second block", genesisBlock.hash);
-        System.out.println("hash for block 2 : " + secondBlock.hash);
+        blockchain.add(new Block("hi i am the second block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("trying to mine block 2 .....");
+        blockchain.get(1).mineBlock(difficulty);
 
-        Block thirdBlock = new Block("hi am the third block", secondBlock.hash);
-        System.out.println("hash for block 3 : " + thirdBlock.hash);
+        blockchain.add(new Block("hi am the third block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("trying to mine block 3.......");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\n Blockchain is valid : " + isChainValid());
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\n The block chain :");
+        System.out.println(blockchainJson);
     }
 }
